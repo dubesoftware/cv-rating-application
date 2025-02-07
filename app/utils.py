d6477ff14794a10c048618bf82e9587d0bcbefd3
dubesoftware <therealdubesoftware@gmail.com>
2025-02-07 13:26:07 +0200

    Fix indentation

--- app/utils.py
+++ app/utils.py
@@ -78,14 +78,14 @@ def get_openai_feedback(text):
             citations.append(f"[{index}] {cited_file.filename}")
     return message_content.value
 
-    def delete_file(file_path):
-        os.remove(file_path)
+def delete_file(file_path):
+    os.remove(file_path)
 
-    def clean_up_resources(file_path):
-        # Clean up uploaded CV PDF and converted .txt files
-        file_paths = [
-            file_path,
-            "converted/converted.txt"
-        ]
-        list(map(delete_file, file_paths))
-        return True
+def clean_up_resources(file_path):
+    # Clean up uploaded CV PDF and converted .txt files
+    file_paths = [
+        file_path,
+        "converted/converted.txt"
+    ]
+    list(map(delete_file, file_paths))
+    return True
