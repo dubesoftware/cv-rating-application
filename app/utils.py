19da12d20cd85b38958080a65542121bf9ddf932
dubesoftware <therealdubesoftware@gmail.com>
2025-02-07 07:59:29 +0200

    Add file conversion and feedback functions

---
+++ app/utils.py
@@ -0,0 +1,16 @@
+import os
+import requests
+import convertapi
+
+
+def convert_pdf_to_text(file_path):
+    # Use ConvertAPI to convert PDF to text
+    convertapi.api_credentials = 'secret_lRQkjhj9nFEPNunH'
+    convertapi.convert('txt', {
+        'File': file_path
+    }, from_format = 'pdf').save_files(os.path.join('uploads', 'converted'))
+    
+
+def get_openai_feedback(text):
+    # To do: Implement OpenAI API call
+    pass
